5a300b847ad19f2ebfdce97348ea1f34fe113428
arel.g.guatno@accenture.com <66598236598>
2018-03-05 05:18:21 +0800

    Draft main activity UI + Intro music

--- app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
+++ app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.appdev.appdev2018;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Full Screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+        playAudio();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopAudio();
+    }
+
+    public void playAudio() {
+        Intent objIntent = new Intent(this, PlayAudio.class);
+        startService(objIntent);
+    }
+
+    public void stopAudio() {
+        Intent objIntent = new Intent(this, PlayAudio.class);
+        stopService(objIntent);
     }
 }
