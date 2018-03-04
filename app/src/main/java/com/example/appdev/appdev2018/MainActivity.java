6b624f1e73c434291e174eac43e34f130931a1b0
arel.g.guatno@accenture.com <66598236598>
2018-03-05 04:14:45 +0800

    Initializing Firebase

--- app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
+++ app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.appdev.appdev2018;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
