dcc97decaa18e4f73d9768f6332f99910cc0361a
arel.g.guatno@accenture.com <66598236598>
2018-03-11 03:42:52 +0800

    Enchanced music class

--- app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
+++ app/src/main/java/com/example/appdev/appdev2018/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.appdev.appdev2018;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    boolean misPlaying = true;
 
+
+    private ServiceConnection Scon = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder) binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(this, MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
         // Full Screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
-        playAudio();
+
+        // Start Music Service
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        doBindService();
     }
+
 
     @Override
     protected void onStop() {
         super.onStop();
-        stopAudio();
+        //mServ.stopMusic();
+        doUnbindService();
     }
 
-    public void playAudio() {
-        Intent objIntent = new Intent(this, PlayAudio.class);
-        startService(objIntent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (misPlaying) {
+            mServ.pauseMusic();
+            misPlaying = false;
+        }
     }
 
-    public void stopAudio() {
-        Intent objIntent = new Intent(this, PlayAudio.class);
-        stopService(objIntent);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!misPlaying) {
+            mServ.resumeMusic();
+            misPlaying = true;
+        }
     }
 }
